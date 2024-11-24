9f1513a5598e82518918f3ec801c9b4f4104cf1a
Saranya-saro <147738076+Saranya-saro@users.noreply.github.com>
2024-11-24 21:39:13 +0530

    Update db.sql

---
+++ db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE school;
+
+USE school;
+
+CREATE TABLE studentdetails (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    class VARCHAR(50),
+    age INT
+);
+
+select * from studentdetails;
